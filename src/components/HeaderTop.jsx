3db65faca3d35608433cc845e600c6853f4cbade
Praveen-2022 <praveenkumarchauhan2022@gmail.com>
2023-09-13 21:12:09 +0530

    final deployement

---
+++ src/components/HeaderTop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//HeaderTop use to set almabetter logo in top
+const HeaderTop = () => {
+  return (
+    <div className=' bg-white shadow-lg py-2 px-2 mx-4 rounded '>
+        <img className='h-6' src="https://www.almabetter.com/_next/image?url=https%3A%2F%2Falmablog-media.s3.ap-south-1.amazonaws.com%2Flogo1_edfc81b31b.png&w=384&q=75" alt="" srcset="" />
+    </div>
+  )
+}
+
+export default HeaderTop
